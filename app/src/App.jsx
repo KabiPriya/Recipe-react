d4d255b542ebbb200a09251da35bd7e7d5feb4f9
KabiPriya <kabipriya.t@gmail.com>
2023-10-11 23:11:52 +0530

    added func

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,50 +1,65 @@
-import React, { useState } from 'react';
-import './App.css';
-import AddRecipe from './components/Addrecipe';
-import Home from './components/Home';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import MovieForm from "./components/MovieForm";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  const [recipes, setRecipes] = useState([]);
-  const [showAddRecipe, setShowAddRecipe] = useState(false);
+function setToLocalStorage(movies) {
+  localStorage.setItem("My-IMDB", JSON.stringify(movies));
+}
 
-  const addRecipe = (newRecipe) => {
-    setRecipes([...recipes, newRecipe]);
-  };
+function getFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("My-IMDB")) || [];
+}
 
+const NavigationBar = (props) => {
+  const { setShowAddMovie } = props;
   return (
-    <div className="app">
-      <nav>
-        <ul>
-          <li>
-            <input
-              type="radio"
-              id="add-recipe"
-              name="view"
-              value="add-recipe"
-              checked={showAddRecipe}
-              onChange={() => setShowAddRecipe(true)}
-            />
-            <label htmlFor="add-recipe">Add Recipe</label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              id="home"
-              name="view"
-              value="home"
-              checked={!showAddRecipe}
-              onChange={() => setShowAddRecipe(false)}
-            />
-            <label htmlFor="home">Home</label>
-          </li>
-        </ul>
-      </nav>
-      {showAddRecipe ? (
-        <AddRecipe addRecipe={addRecipe} />
-      ) : (
-        <Home recipes={recipes} />
-      )}
+    <div className="navBar">
+      <a href="#" onClick={() => setShowAddMovie(false)}>
+        Home
+      </a>
+      <a href="#" onClick={() => setShowAddMovie(true)}>
+        Add Movie
+      </a>
     </div>
+  );
+};
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [showAddMovie, setShowAddMovie] = useState(false);
+
+  useEffect(() => setMovies(getFromLocalStorage()), []);
+  useEffect(() => {
+    if (!movies) {
+      setToLocalStorage(movies);
+    }
+  }, [movies]);
+
+  function handleAddMovie(newMovie) {
+    const updatedMovies = [{ id: uuidv4(), ...newMovie }, ...movies];
+    setMovies(updatedMovies);
+    setToLocalStorage(updatedMovies);
+    setShowAddMovie(false);
+  }
+  return (
+    <>
+      <h1>My IMDB</h1>
+      <NavigationBar setShowAddMovie={setShowAddMovie} />
+      <main>
+        {showAddMovie ? (
+          <div className="formbackground-image">
+            <div id="addMovie">
+              <MovieForm onAddMovie={handleAddMovie} />
+            </div>
+          </div>
+        ) : (
+          <div id="home">
+            <Home movies={movies} />
+          </div>
+        )}
+      </main>
+    </>
   );
 }
 
--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
-function Home({ recipes }) {
+const Home = (props) => {
+  const { movies } = props;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    setFilteredMovies(JSON.parse(localStorage.getItem("My-IMDB")) || []);
+  }, [movies]);
+
+  useEffect(() => {
+    const filtered = movies.filter((movie) =>
+      movie.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+    if (!searchTerm) {
+      setFilteredMovies(movies);
+    }
+    setFilteredMovies(filtered);
+  }, [searchTerm]);
+  const handleSearch = () => {
+    console.log("clicked search");
+  };
   return (
-    <div className="home">
-      <h2>Recipes</h2>
-      <div className="recipe-container">
-        {recipes.map((recipe, index) => (
-          <div key={index} className="recipe-card">
-            <img src={recipe.imageUrl} alt={recipe.name} />
-            <div className="recipe-details">
-              <h3>{recipe.name}</h3>
-              <p>{recipe.steps}</p>
-            </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          id="movie-search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search by movie title..."
+        />
+        <button id="search-button" onClick={handleSearch}>
+          search
+        </button>
+      </div>
+      <div className="movie-cards">
+        {filteredMovies.map((movie, index) => (
+          <div className="movie-card" key={index}>
+            <MovieCard movie={movie} />
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default Home;
