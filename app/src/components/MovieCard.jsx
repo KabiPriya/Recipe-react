140fe547c12e10347398eecb2596bc71857435cc
KabiPriya <kabipriya.t@gmail.com>
2023-10-15 23:10:31 +0530

    context

--- app/src/components/MovieCard.jsx
+++ app/src/components/MovieCard.jsx
@@ -1,33 +1,27 @@
 const MovieCard = (props) => {
-    const { movie } = props;
-    return (
-      <>
-        <img src={movie.image} alt={movie.title} />
-        <h3>{movie.title}</h3>
-        <p>Year: {movie.year}</p>
-        <p>
-          Rating:
-          {(() => {
-            const stars = [];
-            for (let i = 0; i < Number(movie.rating); i++) {
-              stars.push(
-                <span key={i} className="star gold">
-                  ★
-                </span>
-              );
-            }
-            for (let i = Number(movie.rating); i < 5; i++) {
-              stars.push(
-                <span key={i} className="star">
-                  ★
-                </span>
-              );
-            }
-            return stars;
-          })()}
-        </p>
-      </>
-    );
-  };
-  
-  export default MovieCard;
+  const { movie } = props;
+  const rating = Number(movie.rating);
+
+  const stars = Array.from({ length: 5 }, (_, index) => (
+    <span
+      key={index}
+      className={`star ${index < rating ? "gold" : ""}`}
+    >
+      ★
+    </span>
+  ));
+
+  return (
+    <>
+      <img src={movie.image} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>Year: {movie.year}</p>
+      <p>
+        Rating:
+        <div className="star-rating">{stars}</div>
+      </p>
+    </>
+  );
+};
+
+export default MovieCard;
